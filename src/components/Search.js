9d357eb6815c26d80d8d5dee451707256ef949c8
Pavel Koryakin <unclekoin@gmail.com>
2021-06-23 16:18:44 +0300

    feat: create home page

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Search = () => {
+  return (
+    <div className="form-group mb-3">
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter the user's nickname..."
+      />
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
 
 export const Home = () => {
+  const cards = new Array(15)
+    .fill('')
+    .map((_, index) => index);
 
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Fragment>
+      <Search />
+      <div className="row">
+        { cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={ card }>
+              <Card />
+            </div>
+          )
+        })}
+      </div>
+    </Fragment>
   )
 }
